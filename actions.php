e04cc0f3a4602acd43e0a8b1cbe03556d4863de1
KoloWn <31743905+kolown2007@users.noreply.github.com>
2024-05-23 13:26:33 +0800

    Update actions.php

--- actions.php
+++ actions.php
@@ -1,1 +1,16 @@
+<?php
+// Define the directory of your Git repository
+$repo_dir = '/gittest/gittest';
 
+// Define the command you want to execute
+$command = 'git pull origin master';
+
+// Change to the repository directory
+chdir($repo_dir);
+
+// Execute the command
+$output = shell_exec($command);
+
+// Output the result
+echo "<pre>$output</pre>";
+?>
